e05e29d007994f8e6d3a536e58b67e1e1b4b6bb8
Liora Milbaum <liora@lmb.co.il>
2025-06-07 13:52:34 +0300

    pre-commit workflow, isort

--- afula/request.py
+++ afula/request.py
@@ -1,23 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, ValidationError
-import re
+from wtforms import StringField
 from wtforms.validators import DataRequired, Length, Regexp
 
 
 class RequestForm(FlaskForm):
     repo_name = StringField(
-        'Repo Name',
+        "Repo Name",
         validators=[
             DataRequired(),
             Length(min=2, max=20),
-            Regexp(r'^[a-zA-Z0-9\-]+$', message="Only letters, numbers and dashes are allowed")
-        ]
+            Regexp(
+                r"^[a-zA-Z0-9\-]+$",
+                message="Only letters, numbers and dashes are allowed",
+            ),
+        ],
     )
     repo_url = StringField(
-        'Repo Url',
+        "Repo Url",
         validators=[
             DataRequired(),
             Length(min=2, max=50),
-            Regexp(r'(?:https?|git)://(?:www\.)?(github\.com|gitlab\.com)/([\w.-]+/[\w.-]+)(?:\.git)?', message="Git repository URLs from GitHub or GitLab")
-        ]
+            Regexp(
+                r"(?:https?|git)://(?:www\.)?(github\.com|gitlab\.com)/([\w.-]+/[\w.-]+)(?:\.git)?",
+                message="Git repository URLs from GitHub or GitLab",
+            ),
+        ],
     )
