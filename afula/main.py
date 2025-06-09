ac1ec92b500390224135b38c9b11a89ea635a124
Liora Milbaum <lmilbaum@redhat.com>
2025-06-09 11:04:10 +0300

    Request a Repo form (#6)
    Co-authored-by: Liora Milbaum <liora@lmb.co.il>

--- afula/main.py
+++ afula/main.py
@@ -1,8 +1,11 @@
 import configparser
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
+import request
 
 app = Flask(__name__)
+app.config["WTF_CSRF_ENABLED"] = False
 
 
 @app.route("/")
@@ -16,6 +19,26 @@ def list_repositories():
         repository.update(config[section])
         repositories.append(repository)
     return render_template("repositories.html", repositories=repositories)
+
+
+@app.route("/request-repo", methods=["GET", "POST"])
+def request_repo():
+    """Request to onboard a Repo Form"""
+    form = request.RequestForm()
+    if form.validate_on_submit():
+        repo_name = form.repo_name.data
+        repo_url = form.repo_url.data
+
+        print(f"Requested Repository - Name: {repo_name}, Url: {repo_url}")
+
+        return redirect(url_for("success"))
+
+    return render_template("request_repo.html", form=form)
+
+
+@app.route("/success")
+def success():
+    return "Repository request submitted successfully!"
 
 
 if __name__ == "__main__":
