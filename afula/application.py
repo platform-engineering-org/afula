4309bc26c0a137b0dc10e41e1f11dcd1327ea446
Liora Milbaum <lmilbaum@redhat.com>
2025-06-28 16:01:08 +0300

    unit tests (#15)
    Co-authored-by: Liora Milbaum <liora@lmb.co.il>

--- afula/application.py
+++ afula/application.py
@@ -17,29 +17,26 @@ management across multiple projects and teams.
 Author: Liora Milbaum
 """
 
-import configparser
-import os
-
 import flask
 
-import database
-import routes
+from . import config, database, routes
 
 
-def create_app():
+def create_app(test_config=None):
     """Create and configure the Flask application."""
-    DB_HOST = os.environ.get("POSTGRES_HOST", "postgres")
-    DB_NAME = os.environ.get("POSTGRES_DB", "mydb")
-    DB_USER = os.environ.get("POSTGRES_USER", "myuser")
-    DB_PASS = os.environ.get("POSTGRES_PASSWORD", "mypassword")
-
     app = flask.Flask(__name__)
-    app.config["WTF_CSRF_ENABLED"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-        f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
-    )
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config.from_object(config.Config)
+    if test_config:
+        app.config.update(test_config)
+
     database.db.init_app(app)
+
+    @app.route("/")
+    def home():
+        """Non-blueprint home route."""
+        return flask.jsonify(message="Welcome to the Afula app!")
+
     app.register_blueprint(routes.bp)
 
     with app.app_context():
@@ -48,21 +45,6 @@ def create_app():
     return app
 
 
-app = create_app()
-
-
-@app.route("/", methods=["GET"])
-def list_repositories():
-    """List Repositories Page."""
-    config = configparser.ConfigParser()
-    config.read("repositories.cfg")
-    repositories = []
-    for section in config.sections():
-        repository = {"name": section}
-        repository.update(config[section])
-        repositories.append(repository)
-    return flask.render_template("repositories.html", repositories=repositories)
-
-
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
